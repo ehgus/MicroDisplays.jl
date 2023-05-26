cc27704b7a336a56399403c89966ecb638495124
Dohyeon Lee <dleh428@kaist.ac.kr>
2023-05-26 16:36:28 +0900

    capital D

--- src/MicroDisplays.jl
+++ src/MicroDisplays.jl
@@ -1,44 +1,44 @@
-module Microdisplays
+module MicroDisplays
 
 import Base: write!, isopen, size
 
-abstract type Microdisplay end
+abstract type MicroDisplay end
 
 # operation functions
 """
 open microdisplay.
 """
-open!(display::Microdisplay) = error("No implementation for $(typeof(display))")
+open!(display::MicroDisplay) = error("No implementation for $(typeof(display))")
 
 """
 close microdisplay.
 """
-close!(display::Microdisplay) = error("No implementation for $(typeof(display))")
+close!(display::MicroDisplay) = error("No implementation for $(typeof(display))")
 
 """
 start microdisplay.
 """
-start!(display::Microdisplay) = error("No implementation for $(typeof(display))")
+start!(display::MicroDisplay) = error("No implementation for $(typeof(display))")
 
 """
 stop microdisplay.
 """
-stop!(display::Microdisplay) = error("No implementation for $(typeof(display))")
+stop!(display::MicroDisplay) = error("No implementation for $(typeof(display))")
 
 """
 write (or upload) images on microdisplay.
 """
-write!(display::Microdisplay, images_or_files) = error("No implementation for $(typeof(display))")
+write!(display::MicroDisplay, images_or_files) = error("No implementation for $(typeof(display))")
 
 # status functions
 """
 Return if the microdisplay is open.
 """
-isopen(display::Microdisplay) = error("No implementation for $(typeof(display))")
+isopen(display::MicroDisplay) = error("No implementation for $(typeof(display))")
 
 """
 Return the pixels
 """
-size(display::Microdisplay) = error("No implementation for $(typeof(display))")
+size(display::MicroDisplay) = error("No implementation for $(typeof(display))")
 
 end # module MicroDisplays
